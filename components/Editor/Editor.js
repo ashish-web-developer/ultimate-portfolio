774754ba54f5a2555ec8f21df01284f4e8eca0db
Ashish <78073173+ashish-web-developer@users.noreply.github.com>
2022-09-11 23:39:17 +0530

    added header and list

--- components/Editor/Editor.js
+++ components/Editor/Editor.js
@@ -1,6 +1,7 @@
 
 import { useEffect ,useRef,useState,memo} from "react";
 import { makeStyles } from "@mui/styles";
+import { Button ,Grid} from "@mui/material";
 
 
 const useStyles = makeStyles({
@@ -15,21 +16,33 @@ const Editor = ()=>{
   const [editorData,setEditorData] = useState();
   const classes = useStyles();
   const [isRendered,setIsRendered] = useState(false);
+  let editor;
 
   function initEditor(){
-    const Editorjs = require("@editorjs/editorjs")
+    const EditorJS = require("@editorjs/editorjs")
     const Header = require("@editorjs/header");
     const Table = require("@editorjs/table");
+    const List = require("@editorjs/list");
 
-    const editor = new Editorjs({
+    editor = new EditorJS({
       logLevel:"error",
       holder:EDITOR_HOLDER_ID,
       tools:{
         header:Header,
-        table:Table
+        table:Table,
+        list:List
       },
       data:{},
-      placeholder:"Lets write"
+      placeholder:"Lets write",
+    })
+  }
+
+
+  const editorDatahandler = ()=>{
+    editor.save().then((data)=>{
+      console.log("value of data",data);
+    }).catch((err)=>{
+      console.log("value of err",err);
     })
   }
 
@@ -44,7 +57,16 @@ const Editor = ()=>{
   },[isRendered])
 
   return (
-    <div className = {classes.editorContainer} id = {EDITOR_HOLDER_ID}>
+    <div>
+      <Grid  container>
+        <Grid item xs = {8}>
+          <div className = {classes.editorContainer} id = {EDITOR_HOLDER_ID}>
+          </div>
+        </Grid>
+        <Grid item xs = {4}>
+          <Button onClick = {editorDatahandler} variant = "contained">Save</Button>
+        </Grid>
+      </Grid>
     </div>
   )
 }
--- pages/blog-edit.js
+++ pages/blog-edit.js
@@ -1,14 +1,54 @@
-import dynamic from "next/dynamic";
 import { useEffect ,useState} from "react";
 import Editor from "../components/Editor/Editor";
 import { makeStyles } from "@mui/styles";
+import {Grid,Button,IconButton} from "@mui/material";
+import {FiChevronDown} from "react-icons/fi"
 
 
 const useStyles = makeStyles({
     container:{
         backgroundColor:"#fff",
-        color:"#000"
-    }
+        color:"#000",
+        minHeight:"100vh",
+        padding:"0px 3rem"
+    },
+    header:{
+        height:"100px",
+        borderBottom:"2px solid #e2e3e6",
+        marginBottom:"2rem",
+        display:"flex",
+        alignItems:"center"
+    },
+    logoContainer:{
+        display:"flex",
+        alignItems:"flex-start",
+    },
+    logoCanvasContainer:{
+        width:"70px",
+        height:"70px"
+    },
+    logoText:{
+        fontFamily:"'Bungee', cursive",
+        color:"#000",
+        fontSize:"20px",
+        textDecoration:"underline",
+        textDecorationColor:"#e2cf52",
+        textDecorationThickness:"5px"
+    },
+    curlyBraces:{
+        color:"#e2cf52"
+    },
+    publishCta:{
+        "&.MuiButton-root":{
+            color:"#e2cf52",
+            fontFamily:"'Oswald', sans-serif;",
+            backgroundColor:"#101727",
+            padding:"0.5rem 0px"
+        },
+        "&.MuiButton-root:hover":{
+            backgroundColor:"#101727",
+        }
+    },
 })
 
 
@@ -21,6 +61,21 @@ const BlogEdit = ()=>{
     return(
         isLoaded && 
         <div className = {classes.container}>
+            <div className = {classes.header}>
+                <Grid container>
+                    <Grid item xs = {10}>
+                        <div className = {classes.logoContainer}>
+                            <span className = {classes.logoText}><span className = {classes.curlyBraces}> &#123;</span> Ashish Prajapati <span className = {classes.curlyBraces}>&#125;</span></span>
+                        </div>
+                    </Grid>
+                    <Grid item xs = {1}>
+                        <IconButton><FiChevronDown color = "#000" size = {30}/></IconButton>
+                    </Grid>
+                    <Grid item xs = {1}>
+                        <Button fullWidth className = {classes.publishCta} variant = "contained">Publish</Button>
+                    </Grid>
+                </Grid>
+            </div>
             <Editor/>
         </div>
     )
