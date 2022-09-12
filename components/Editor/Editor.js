25e711514a7c0f858d96d7e4d45c46bf8f8b6d11
Ashish <78073173+ashish-web-developer@users.noreply.github.com>
2022-09-12 15:55:27 +0530

    design fixes

--- components/Editor/Editor.js
+++ components/Editor/Editor.js
@@ -1,21 +1,45 @@
 
 import { useEffect ,useRef,useState,memo} from "react";
 import { makeStyles } from "@mui/styles";
-import { Button ,Grid} from "@mui/material";
+import { Button ,Grid,Tabs,Tab,Box} from "@mui/material";
 
 
 const useStyles = makeStyles({
   editorContainer:{
     backgroundColor:"#fff"
+  },
+  sidePanel:{
+    border:"2px solid #e2e3e6",
+    minHeight:"500px"
+  },
+  sidePanelHeader:{
+    height:"50px"
+  },
+  tabs:{
+    "&.MuiTab-root":{
+      fontFamily:"'Oswald', sans-serif;",
+      textTransform:"capitalize"
+    },
+    "&.Mui-selected": {
+      color: '#e2cf52 !important'
+    }
+
   }
 })
 
 
 const EDITOR_HOLDER_ID = "editorjs";
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 const Editor = ()=>{
   const [editorData,setEditorData] = useState();
   const classes = useStyles();
   const [isRendered,setIsRendered] = useState(false);
+  const [tabValue,setTabValue] = useState(0);
   let editor;
 
   function initEditor(){
@@ -23,6 +47,8 @@ const Editor = ()=>{
     const Header = require("@editorjs/header");
     const Table = require("@editorjs/table");
     const List = require("@editorjs/list");
+    const Code = require("@editorjs/code");
+    const Embed = require("@editorjs/embed");
 
     editor = new EditorJS({
       logLevel:"error",
@@ -30,7 +56,17 @@ const Editor = ()=>{
       tools:{
         header:Header,
         table:Table,
-        list:List
+        list:List,
+        code:Code,
+        embed: {
+          class: Embed,
+          config: {
+            services: {
+              youtube: true,
+              coub: true
+            }
+          }
+        },
       },
       data:{},
       placeholder:"Lets write",
@@ -44,6 +80,9 @@ const Editor = ()=>{
     }).catch((err)=>{
       console.log("value of err",err);
     })
+  }
+  const tabPanelHandler = (event,newValue)=>{
+    setTabValue(newValue);
   }
 
 
@@ -59,12 +98,20 @@ const Editor = ()=>{
   return (
     <div>
       <Grid  container>
-        <Grid item xs = {8}>
+        <Grid item xs = {9}>
           <div className = {classes.editorContainer} id = {EDITOR_HOLDER_ID}>
           </div>
         </Grid>
-        <Grid item xs = {4}>
-          <Button onClick = {editorDatahandler} variant = "contained">Save</Button>
+        <Grid item xs = {3}>
+          <div className = {classes.sidePanel}>
+            <Box className = {classes.sidePanelHeader}>
+              <Tabs TabIndicatorProps  = {{style:{backgroundColor:"#e2cf52"}}}  value={tabValue} onChange = {tabPanelHandler} aria-label="basic tabs example">
+                <Tab className = {classes.tabs} label="Post Settings" {...a11yProps(0)} />
+                <Tab className = {classes.tabs} label="Item Two" {...a11yProps(1)} />
+                <Tab className = {classes.tabs} label="Item Three" {...a11yProps(2)} />
+              </Tabs>
+            </Box>
+          </div>
         </Grid>
       </Grid>
     </div>
