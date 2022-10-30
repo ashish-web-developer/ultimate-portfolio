67abd81f0a70a31f33a47d8da3ebca2ab34fa4ad
Ashish Prajapati <beingsocasual@gmail.com>
2022-10-30 23:11:24 +0530

    added loaader

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,6 +14,7 @@ import useStyles from "../styles/login";
 
 // React Three js
 import { Canvas } from "@react-three/fiber";
+import {Loader} from "@react-three/drei";
 
 
 
@@ -27,33 +28,36 @@ const Login:NextPage = ()=>{
     },[])
     return (
         isLoaded && 
-        <div className = {classes.container}>
-            <Grid sx = {{height:"100%"}} container>
-                <Grid item xs = {5}>
-                    <SignUpLogin/>
-                </Grid>
-                <Grid item xs = {7}>
-                    <div className = {classes.loginRHS}>
-                        <div className = {classes.modelContainer}>
-                            <Canvas>
-                                <ambientLight intensity={0.5} />
-                                <pointLight position={[-10, 10, -5]} />
-                                <spotLight intensity={0.5} position={[0, -10, -10]} />
-                                <Suspense>
-                                    <PhonixBird rotation={[0, 3.5, 0]} position={[-3,-100,-700]} />
-                                </Suspense>
-                            </Canvas>
-                        </div>
-                        <div className = {classes.loginRHSContentContainer}>
-                            <h1 className = {classes.loginRHSTitle}>Lets Expore New World</h1>
-                            <div className ={classes.subtitle}>
-                                You will get unlimited access to all blog, and you can create your own personal blog free, can contact me also, for any further query.
+        <>
+            <div className = {classes.container}>
+                <Grid sx = {{height:"100%"}} container>
+                    <Grid item xs = {5}>
+                        <SignUpLogin/>
+                    </Grid>
+                    <Grid item xs = {7}>
+                        <div className = {classes.loginRHS}>
+                            <div className = {classes.modelContainer}>
+                                <Canvas>
+                                    <ambientLight intensity={0.5} />
+                                    <pointLight position={[-10, 10, -5]} />
+                                    <spotLight intensity={0.5} position={[0, -10, -10]} />
+                                    <Suspense>
+                                        <PhonixBird rotation={[0, 3.5, 0]} position={[-3,-100,-700]} />
+                                    </Suspense>
+                                </Canvas>
+                            </div>
+                            <div className = {classes.loginRHSContentContainer}>
+                                <h1 className = {classes.loginRHSTitle}>Lets Expore New World</h1>
+                                <div className ={classes.subtitle}>
+                                    You will get unlimited access to all blog, and you can create your own personal blog free, can contact me also, for any further query.
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </div>
+            </div>
+            <Loader/>
+        </>
     )
 }
 
