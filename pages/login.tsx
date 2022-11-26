0909fa229f0d489136c1b81676c5511f8d4a1bf4
Ashish Prajapati <beingsocasual@gmail.com>
2022-11-26 20:30:40 +0530

    minor changes

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,18 +1,21 @@
 // React
 import { NextPage } from "next";
 import {useEffect, useState , Suspense} from "react";
+import dynamic from "next/dynamic";
 // Mui
 import { Grid,useMediaQuery } from "@mui/material";
 
-// Component
+// Local Component
 import SignUpLogin from "../components/Login/SignupLogin";
-import PhonixBird from "../components/Login/Phoenix_bird";
+
+const Model = dynamic(()=>import("../components/Login/Model"),{
+    ssr:false
+})
 
 // styles
 import useStyles from "../styles/login";
 
 // React Three js
-import { Canvas } from "@react-three/fiber";
 import {Loader} from "@react-three/drei";
 
 
@@ -37,24 +40,7 @@ const Login:NextPage = ()=>{
                     </Grid>
                     {
                         !isMobile &&<Grid item xs = {12} md={7}>
-                            <div className = {classes.loginRHS}>
-                                <div className = {classes.modelContainer}>
-                                    <Canvas>
-                                        <ambientLight intensity={0.5} />
-                                        <pointLight position={[-10, 10, -5]} />
-                                        <spotLight intensity={0.5} position={[0, -10, -10]} />
-                                        <Suspense>
-                                            <PhonixBird rotation={[0, 3.5, 0]} position={[-3,-100,-700]} />
-                                        </Suspense>
-                                    </Canvas>
-                                </div>
-                                <div className = {classes.loginRHSContentContainer}>
-                                    <h1 className = {classes.loginRHSTitle}>Lets Expore New World</h1>
-                                    <div className ={classes.subtitle}>
-                                        You will get unlimited access to all blog, and you can create your own personal blog free, can contact me also, for any further query.
-                                    </div>
-                                </div>
-                            </div>
+                            <Model classes = {classes}/>
                         </Grid>
                     }
                 </Grid>
