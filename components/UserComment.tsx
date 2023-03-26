bdc2b723564895aa394c9619f126e64170f88ae2
Ashish Prajapati <beingsocasual@gmail.com>
2023-03-26 22:33:34 +0530

    [comment-functionality] some ui fixes

--- components/UserComment.tsx
+++ components/UserComment.tsx
@@ -2,15 +2,56 @@
 import { createAvatar } from '@dicebear/core';
 import { lorelei } from '@dicebear/collection';
 
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+    container:{
+        padding:"20px 0px",
+        display:"flex",
+        flexDirection:"column"
+    },
+    userContainer:{
+        display:"flex",
+        alignItems:"center",
+        gap:"20px"
+    },
+    avatarContainer:{
+        width:"50px",
+        height:"50px",
+        border:"3px solid #fff",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        borderRadius:"50%"
+    },
+    userName:{
+        fontFamily: "'Bungee', cursive",
+    },
+    userComment:{
+        fontFamily:"'Allerta Stencil', sans-serif",
+        marginLeft:"68px"
+    }
+})
+
 const UserComment = ({comment,user})=>{
+    const classes = useStyles();
     const avatar = createAvatar(lorelei, {
         seed: user?.name,
         // ... other options
     });
     return (
-        <div>
-            <div style = {{width:"30px",height:"30px"}} dangerouslySetInnerHTML={{ __html: avatar.toString() }} />
-            <span>{comment.body}</span>
+        <div className = {classes.container}>
+            <div className = {classes.userContainer}>
+                <div className={classes.avatarContainer} >
+                    <div style = {{width:"40px",height:"40px"}} dangerouslySetInnerHTML={{ __html: avatar.toString() }} />
+                </div>
+                <div className = {classes.userName}>
+                    {user.name}
+                </div>
+            </div>
+            <div className = {classes.userComment}>
+                {comment.body}
+            </div>
         </div>
     )
 }
